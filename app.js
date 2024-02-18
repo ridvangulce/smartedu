8e900adb9411f43307860290f220bd34f628b176
ridvangulce <ridvangulce@gmail.com>
2024-02-18 14:22:54 +0300

    +added views directory and create partials

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const express = require('express');
 
 const app = express();
-const port = 5001;
 
-app.use(express.json()); // for parsing application/json
-// parse requests of content-type - application/x-www-form-urlencoded
+//Template Engine
+app.set('view engine', 'ejs');
+
+//Middlewares
+app.use(express.static('public'));
+
+app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
 
-// simple route
+// Routers
 app.get('/', (req, res) => {
-   res.status(200).send('Welcome!');
+   res.status(200).render('index', {
+      page_name: 'index',
+   });
+});
+app.get('/about', (req, res) => {
+   res.status(200).render('about', {
+      page_name: 'about',
+   });
 });
 
+const port = 5001;
 app.listen(port, () => {
    console.log(`Server is running at http://localhost:${port}`);
 });
