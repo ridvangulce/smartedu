ab3129585a3a9888d1aa0c551c2e34af9e582554
ridvangulce <ridvangulce@gmail.com>
2024-02-20 17:31:39 +0300

    +added login process, check email and bcrpyted password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,8 @@
 const User = require('../models/User.js');
-
+const bcrypt = require('bcrypt');
 exports.createUser = async (req, res) => {
-   const user = await User.create(req.body);
    try {
+      const user = await User.create(req.body);
       res.status(201).json({
          status: 'success',
          user,
@@ -11,6 +11,29 @@ exports.createUser = async (req, res) => {
       return res.status(400).json({
          status: 'fail',
          message: error,
+      });
+   }
+};
+
+exports.loginUser = async (req, res) => {
+   try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+      if (!user) {
+         return res.status(404).json({ message: 'User not found' });
+      }
+
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (!passwordMatch) {
+         return res.status(401).json({ message: 'Invalid password' });
+      }
+
+      // User Session
+      res.status(200).json({ message: 'Logged In' });
+   } catch (error) {
+      return res.status(400).json({
+         status: 'fail',
+         message: error.message,
       });
    }
 };
